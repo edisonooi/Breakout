098262d0721e8f8c7507cd11ba613f0123808702
Edison Ooi <edison.ooi@duke.edu>
2022-01-11 00:32:50 -0500

    Created constants for balls and paddles

--- src/main/java/breakout/Breakout.java
+++ src/main/java/breakout/Breakout.java
@@ -1,5 +1,10 @@
 package breakout;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
 /**
  * The class that manages the entire game, including dealing with player
  * interactions and setting up visuals
@@ -7,5 +12,15 @@ package breakout;
  * @author Edison Ooi
  */
 public class Breakout {
+    //Default settings for balls
+    public static final int BALL_SIZE = 30;
+    public static final Paint BALL_COLOR = Color.WHITE;
+
+    //Default settings for paddle
+    public static final int PADDLE_WIDTH = 50;
+    public static final int PADDLE_HEIGHT = 10;
+    public static final Paint PADDLE_COLOR = Color.WHITE;
+
+
 
 }
