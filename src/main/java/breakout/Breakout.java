fcb6c060847c2aff53fe909ef6e512a8dc9d7fcf
Edison Ooi <edison.ooi@duke.edu>
2022-01-11 00:16:30 -0500

    Created Breakout class

---
+++ src/main/java/breakout/Breakout.java
@@ -0,0 +1,11 @@
+package breakout;
+
+/**
+ * The class that manages the entire game, including dealing with player
+ * interactions and setting up visuals
+ *
+ * @author Edison Ooi
+ */
+public class Breakout {
+
+}
