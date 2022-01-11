44e36bdf800ff1dcd07df0606b234188d7b4cd2c
Edison Ooi <edison.ooi@duke.edu>
2022-01-11 14:27:08 -0500

    Rendered paddle onto screen

--- src/main/java/breakout/Breakout.java
+++ src/main/java/breakout/Breakout.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 /**
  * The class that manages the entire game, including dealing with player
@@ -24,9 +25,13 @@ public class Breakout {
     public static final int PADDLE_HEIGHT = 10;
     public static final Paint PADDLE_COLOR = Color.WHITE;
 
+    //Main ball
     private Circle myBall;
     private int ballXDirection = 1;
     private int ballYDirection = 1;
+
+    //Main paddle
+    private Rectangle myPaddle;
 
     private int sceneWidth;
     private int sceneHeight;
@@ -41,8 +46,14 @@ public class Breakout {
         myBall.setCenterX(width / 2);
         myBall.setCenterY(height / 2);
 
+        //Initialize paddle toward bottom of screen
+        myPaddle = new Rectangle(PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_COLOR);
+        myPaddle.setX(width / 2 - myPaddle.getWidth() / 2);
+        myPaddle.setY(height - height / 8 - myPaddle.getHeight() / 2);
+
         //Add subviews to group
         root.getChildren().add(myBall);
+        root.getChildren().add(myPaddle);
 
         //Create main scene and set instance width and height
         Scene scene = new Scene(root, width, height, background);
