54b18549cd64b8785d17b06700918bc1bb7f6290
Edison Ooi <edison.ooi@duke.edu>
2022-01-16 13:20:48 -0500

    Added default paddle speed

--- src/main/java/breakout/Paddle.java
+++ src/main/java/breakout/Paddle.java
@@ -4,9 +4,12 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
 public class Paddle extends Rectangle {
+    public static final int PADDLE_SPEED = 10;
     private int width;
     private int height;
     private Paint color;
+
+
 
     public Paddle(double width, double height, Paint color) {
         super(width, height, color);
