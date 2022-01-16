d36fbdffb52821c1e2a3211208216845ffedfaf7
Edison Ooi <edison.ooi@duke.edu>
2022-01-16 17:07:02 -0500

    Moved move() functionality into Paddle class

--- src/main/java/breakout/Paddle.java
+++ src/main/java/breakout/Paddle.java
@@ -2,21 +2,46 @@ package breakout;
 
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.input.KeyCode;
 
 public class Paddle extends Rectangle {
-    public static final int PADDLE_SPEED = 10;
-    private int width;
-    private int height;
-    private Paint color;
+    // Default paddle speed
+    public static final int PADDLE_SPEED = 20;
 
+    // Instance variables
+    private boolean isHorizontal;
 
-
-    public Paddle(double width, double height, Paint color) {
+    /**
+     * Class constructor
+     * @param width Width of paddle
+     * @param height Height of paddle
+     * @param isHorizontal Boolean indicating if paddle moves along x-axis
+     * @param color Color of paddle
+     */
+    public Paddle(double width, double height, boolean isHorizontal, Paint color) {
         super(width, height, color);
-
+        this.isHorizontal = isHorizontal;
     }
 
-
+    /**
+     * Updates paddle position based on key pressed
+     * @param code KeyCode of key that is pressed
+     */
+    public void move(KeyCode code) {
+        if(isHorizontal) {
+            if(code == KeyCode.LEFT) {
+                this.setX(this.getX() - PADDLE_SPEED);
+            } else if(code == KeyCode.RIGHT) {
+                this.setX(this.getX() + PADDLE_SPEED);
+            }
+        } else {
+            if(code == KeyCode.UP) {
+                this.setY(this.getY() - PADDLE_SPEED);
+            } else if(code == KeyCode.DOWN) {
+                this.setY(this.getY() + PADDLE_SPEED);
+            }
+        }
+    }
 
 
 }
