03f1c2eb9d6f80db2fb3707ede241c5e07c21ec7
Edison Ooi <edison.ooi@duke.edu>
2022-01-16 13:13:13 -0500

    Created Paddle class

---
+++ src/main/java/breakout/Paddle.java
@@ -0,0 +1,19 @@
+package breakout;
+
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
+public class Paddle extends Rectangle {
+    private int width;
+    private int height;
+    private Paint color;
+
+    public Paddle(double width, double height, Paint color) {
+        super(width, height, color);
+
+    }
+
+
+
+
+}
