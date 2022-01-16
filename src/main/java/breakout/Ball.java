4eac209770f6af639766a762d6c485927bc0aad6
Edison Ooi <edison.ooi@duke.edu>
2022-01-16 16:07:07 -0500

    Modified constructor, deleted unneeded instance variables, added comments

--- src/main/java/breakout/Ball.java
+++ src/main/java/breakout/Ball.java
@@ -8,20 +8,30 @@ import javafx.scene.shape.Rectangle;
 import java.util.Set;
 
 public class Ball extends Circle {
-    private double radius;
-
+    // Instance variables
     private int xVelocity;
     private int yVelocity;
 
-    private Paint color;
-
+    /**
+     * Class constructor
+     * @param radius radius of ball, in pixels
+     * @param xVelocity velocity of ball in x direction
+     * @param yVelocity velocity of ball in x direction
+     * @param color color of ball
+     */
     public Ball(double radius, int xVelocity, int yVelocity, Paint color) {
-        this.radius = radius;
+        super(radius, color);
         this.xVelocity = xVelocity;
         this.yVelocity = yVelocity;
-        this.color = color;
     }
 
+    /**
+     * Updates properties of ball and brick upon collision
+     * @param brick brick that is colliding with ball
+     * @param root Group of Nodes being rendered by scene
+     * @param bricks Set of remaining bricks in current level
+     * @return powerup assigned to brick if it breaks, or null if it doesn't break
+     */
     public Powerup bounce(Brick brick, Group root, Set<Brick> bricks) {
         updateVelocities(brick);
 
@@ -36,10 +46,18 @@ public class Ball extends Circle {
         return null;
     }
 
+    /**
+     * Updates properties of ball upon colliding with paddle
+     * @param paddle Paddle that ball collides with
+     */
     public void bounce(Paddle paddle) {
         updateVelocities(paddle);
     }
 
+    /**
+     * Updates velocities of ball depending on which side of rectangle it bounces off of
+     * @param rect Rectangle that ball collides with
+     */
     private void updateVelocities(Rectangle rect) {
 
     }
