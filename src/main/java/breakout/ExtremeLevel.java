cf63b08b210914dae7738f633fdedc2368fbb357
Edison Ooi <edison.ooi@duke.edu>
2022-01-17 00:37:01 -0500

    Implemented setupChildNodes

--- src/main/java/breakout/ExtremeLevel.java
+++ src/main/java/breakout/ExtremeLevel.java
@@ -19,12 +19,25 @@ public class ExtremeLevel extends Level {
 
     @Override
     public void setupChildNodes(Group root, int sceneWidth, int sceneHeight) throws FileNotFoundException {
-        leftPaddle = new Paddle(sceneWidth / 20.0, sceneHeight / 5.0, false);
-        rightPaddle = new Paddle(sceneWidth / 20.0, sceneHeight / 5.0, false);
-        topPaddle = new Paddle(sceneWidth / 5.0, sceneHeight / 20.0, true);
-        bottomPaddle = new Paddle(sceneWidth / 5.0, sceneHeight / 20.0, true);
+        leftPaddle = new Paddle(sceneWidth / 5.0, sceneHeight / 20.0, true);
+        leftPaddle.setX(sceneWidth * 0.1 - leftPaddle.getWidth());
+        leftPaddle.setY(sceneHeight * 0.5 - leftPaddle.getHeight() / 2);
+
+        rightPaddle = new Paddle(sceneWidth / 5.0, sceneHeight / 20.0, true);
+        rightPaddle.setX(sceneWidth * 0.9);
+        rightPaddle.setY(sceneHeight * 0.5 - bottomPaddle.getHeight() / 2);
+
+        topPaddle = new Paddle(sceneWidth / 20.0, sceneHeight / 5.0, false);
+        topPaddle.setX(sceneWidth * 0.5 - leftPaddle.getWidth() / 2);
+        topPaddle.setY(sceneHeight * 0.1 - leftPaddle.getHeight());
+
+        bottomPaddle = new Paddle(sceneWidth / 20.0, sceneHeight / 5.0, false);
+        bottomPaddle.setX(sceneWidth * 0.5 - rightPaddle.getWidth() / 2);
+        bottomPaddle.setY(sceneHeight * 0.9);
 
         myBall = new Ball(150, 100);
+        myBall.setCenterX(sceneWidth * 0.5);
+        myBall.setCenterY(sceneHeight * 0.2);
 
         setupBricks(root, sceneWidth / 4, sceneHeight / 4,
                 sceneWidth - sceneWidth / 4, sceneHeight - sceneHeight / 4);
