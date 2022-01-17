20d365bde5b064ba0884a28474d8801488648fce
Edison Ooi <edison.ooi@duke.edu>
2022-01-17 16:01:59 -0500

    Rendered all components and level is playable now

--- src/main/java/breakout/ExtremeLevel.java
+++ src/main/java/breakout/ExtremeLevel.java
@@ -13,31 +13,37 @@ public class ExtremeLevel extends Level {
 
     private Ball myBall;
 
-    public ExtremeLevel(int levelNumber, int lives) {
-        super(levelNumber, lives);
+    public ExtremeLevel(int levelNumber, int lives, Group root, int sceneWidth, int sceneHeight) {
+        super(levelNumber, lives, root, sceneWidth, sceneHeight);
     }
 
     @Override
-    public void setupChildNodes(Group root, int sceneWidth, int sceneHeight) throws FileNotFoundException {
-        leftPaddle = new Paddle(sceneWidth / 5.0, sceneHeight / 20.0, true);
+    public void setupChildNodes(Group root, int sceneWidth, int sceneHeight) {
+        leftPaddle = new Paddle(sceneWidth / 20.0, sceneHeight / 5.0, false);
         leftPaddle.setX(sceneWidth * 0.1 - leftPaddle.getWidth());
         leftPaddle.setY(sceneHeight * 0.5 - leftPaddle.getHeight() / 2);
 
-        rightPaddle = new Paddle(sceneWidth / 5.0, sceneHeight / 20.0, true);
+        rightPaddle = new Paddle(sceneWidth / 20.0, sceneHeight / 5.0, false);
         rightPaddle.setX(sceneWidth * 0.9);
-        rightPaddle.setY(sceneHeight * 0.5 - bottomPaddle.getHeight() / 2);
+        rightPaddle.setY(sceneHeight * 0.5 - rightPaddle.getHeight() / 2);
 
-        topPaddle = new Paddle(sceneWidth / 20.0, sceneHeight / 5.0, false);
-        topPaddle.setX(sceneWidth * 0.5 - leftPaddle.getWidth() / 2);
-        topPaddle.setY(sceneHeight * 0.1 - leftPaddle.getHeight());
+        topPaddle = new Paddle(sceneWidth / 5.0, sceneHeight / 20.0, true);
+        topPaddle.setX(sceneWidth * 0.5 - topPaddle.getWidth() / 2);
+        topPaddle.setY(sceneHeight * 0.1 - topPaddle.getHeight());
 
-        bottomPaddle = new Paddle(sceneWidth / 20.0, sceneHeight / 5.0, false);
-        bottomPaddle.setX(sceneWidth * 0.5 - rightPaddle.getWidth() / 2);
+        bottomPaddle = new Paddle(sceneWidth / 5.0, sceneHeight / 20.0, true);
+        bottomPaddle.setX(sceneWidth * 0.5 - bottomPaddle.getWidth() / 2);
         bottomPaddle.setY(sceneHeight * 0.9);
 
         myBall = new Ball(150, 100);
         myBall.setCenterX(sceneWidth * 0.5);
         myBall.setCenterY(sceneHeight * 0.2);
+
+        root.getChildren().add(leftPaddle);
+        root.getChildren().add(rightPaddle);
+        root.getChildren().add(topPaddle);
+        root.getChildren().add(bottomPaddle);
+        root.getChildren().add(myBall);
 
         setupBricks(root, sceneWidth / 4, sceneHeight / 4,
                 sceneWidth - sceneWidth / 4, sceneHeight - sceneHeight / 4);
@@ -45,6 +51,29 @@ public class ExtremeLevel extends Level {
 
     @Override
     public void handleKeyInput(KeyCode code) {
+        leftPaddle.move(code);
+        rightPaddle.move(code);
+        topPaddle.move(code);
+        bottomPaddle.move(code);
+    }
 
+    @Override
+    public void step(double elapsedTime) {
+        myBall.move(elapsedTime);
+        checkPaddleCollisions();
+        checkBrickCollisions(myBall);
+        checkWallCollisions(myBall);
+    }
+
+    private void checkPaddleCollisions() {
+        if(Breakout.isIntersecting(leftPaddle, myBall)) {
+            myBall.bounce(leftPaddle);
+        } else if (Breakout.isIntersecting(rightPaddle, myBall)) {
+            myBall.bounce(rightPaddle);
+        } else if (Breakout.isIntersecting(topPaddle, myBall)) {
+            myBall.bounce(topPaddle);
+        } else if (Breakout.isIntersecting(bottomPaddle, myBall)) {
+            myBall.bounce(bottomPaddle);
+        }
     }
 }
