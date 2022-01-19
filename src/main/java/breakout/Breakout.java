56732853c731c5c8ae298b842bc6a1e8c9419f25
Edison Ooi <edison.ooi@duke.edu>
2022-01-18 21:05:28 -0500

    Fixed up comments, cleaned up unneeded variables

--- src/main/java/breakout/Breakout.java
+++ src/main/java/breakout/Breakout.java
@@ -3,74 +3,78 @@ package breakout;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
-import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * The class that manages the entire game, including dealing with player
- * interactions and setting up visuals
+ * interactions and setting up visuals.
  *
  * @author Edison Ooi
  */
 public class Breakout {
-    // How much space to put between each brick
+    // Game-wide constants
     public static final int BRICK_PADDING = 5;
     public static final int INVIS_PADDLE_DURATION = 2000;
     public static final int FAST_PADDLE_DURATION = 5000;
     public static final int SLOW_BALL_DURATION = 5000;
     public static final int SCOREBOARD_HEIGHT = 100;
 
+    // UI members for this instance
     private Scene mainScene;
     private Group root;
     private Scoreboard scoreboard;
 
+    // Variables to track current level being played
     private Level currentLevel;
     private int currentLevelNum;
 
-    private int sceneWidth;
-    private int sceneHeight;
-
+    /**
+     * Creates main scene and Group in which all UI components will live. Also initializes first level and scoreboard,
+     * effectively starting gameplay.
+     *
+     * This should be the first method called after initialization of Breakout object.
+     *
+     * @param width width, in pixels, of playable region of Breakout game
+     * @param height height, in pixels, of playable region of Breakout game
+     * @param background background color of Breakout game
+     * @return Scene object encapsulating all UI components to be rendered in Main
+     */
     public Scene setupGame(int width, int height, Paint background) {
-        sceneWidth = width;
-        sceneHeight = height;
-
-        //Top level collection that encapsulates all subviews in scene
+        // Top level collection that encapsulates all subviews in scene
         root = new Group();
 
-        //Setup scoreboard
-        scoreboard = new Scoreboard(sceneWidth, SCOREBOARD_HEIGHT, "Hello");
+        // Setup scoreboard
+        scoreboard = new Scoreboard(width, SCOREBOARD_HEIGHT);
         scoreboard.setX(0);
         scoreboard.setY(0);
         root.getChildren().add(scoreboard);
 
-        //Initialize first level
+        // Initialize first level
         currentLevelNum = 1;
         Level level1 = new NormalLevel(1, 3, root, width, height, scoreboard);
         currentLevel = level1;
 
+        // Create main scene
+        Scene scene = new Scene(root, width, height + SCOREBOARD_HEIGHT, background);
 
-        //Create main scene
-        Scene scene = new Scene(root, sceneWidth, sceneHeight + SCOREBOARD_HEIGHT, background);
-        //Respond to input
+        // Respond to input
         scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
         mainScene = scene;
 
         return scene;
     }
 
-    // Update all properties after a certain time interval
-    // This serves as a way to animate the objects in game
+    /**
+     * Recalculate all properties of current level and checks if the level is either finished or failed.
+     * This method assumes setupGame() has already been called or currentLevel has been initialized otherwise.
+     *
+     * @param elapsedTime amount of time since last update
+     */
     public void step(double elapsedTime) {
         currentLevel.step(elapsedTime);
 
@@ -84,9 +88,14 @@ public class Breakout {
         } else if (currentLevel.didFail()) {
             showEndScreen(false);
         }
-
     }
 
+    /**
+     * Performs action for global cheat keys, or calls current level's key handler if key was not a global cheat key.
+     * This method assumes setupGame() has already been called or currentLevel has been initialized otherwise.
+     *
+     * @param code KeyCode of key that was pressed
+     */
     public void handleKeyInput(KeyCode code) {
         switch (code) {
             case DIGIT1 -> goToLevel(1);
@@ -110,9 +119,16 @@ public class Breakout {
         }
     }
 
+    // Skips to level of given number, or final level if given level is higher than the highest level.
     private void goToLevel(int level) {
+        System.out.println(mainScene.getWidth());
+        System.out.println(mainScene.getHeight());
+
         currentLevel.clear();
         currentLevelNum = level;
+
+        int sceneWidth = (int) mainScene.getWidth();
+        int sceneHeight = (int) mainScene.getHeight();
 
         switch(currentLevelNum) {
             case 1 -> currentLevel = new NormalLevel(1, 3, root, sceneWidth, sceneHeight, scoreboard);
@@ -121,25 +137,32 @@ public class Breakout {
         }
     }
 
+    // Shows end screen with result and final score, indicating end of game.
     private void showEndScreen(boolean didWin) {
         mainScene.setOnKeyPressed(null);
 
         root.getChildren().clear();
 
-        Text endScreenText = new Text(sceneWidth, sceneHeight, "");
+        Text endScreenText = new Text(mainScene.getWidth(), mainScene.getHeight(), "");
         String endScreenMessage = String.format("You %s\nScore: %d", didWin ? "Win" : "Lose", scoreboard.getScore());
         endScreenText.setText(endScreenMessage);
         endScreenText.setFont(new Font(50));
         endScreenText.setTextAlignment(TextAlignment.CENTER);
         endScreenText.setFill(didWin ? Color.LIMEGREEN : Color.RED);
-        endScreenText.setX(sceneWidth / 2 - endScreenText.getBoundsInLocal().getWidth() / 2);
-        endScreenText.setY((sceneHeight + SCOREBOARD_HEIGHT) / 2 - endScreenText.getBoundsInLocal().getHeight() / 2);
+        endScreenText.setX(mainScene.getWidth() / 2 - endScreenText.getBoundsInLocal().getWidth() / 2);
+        endScreenText.setY((mainScene.getHeight() + SCOREBOARD_HEIGHT) / 2 - endScreenText.getBoundsInLocal().getHeight() / 2);
 
         root.getChildren().add(endScreenText);
     }
 
+    /**
+     * Utility method to determine if two Shapes are intersecting.
+     *
+     * @param a first Shape object
+     * @param b second Shape object
+     * @return boolean indicating if the bounds of the two Shapes are intersecting
+     */
     public static boolean isIntersecting(Shape a, Shape b) {
-        //If the bounds of both shapes intersect, return true
         return b.getBoundsInLocal().intersects(a.getBoundsInLocal());
     }
 }
