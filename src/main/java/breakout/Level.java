808df39af4057d257244b8e6da84d6fc67647615
Edison Ooi <edison.ooi@duke.edu>
2022-01-18 21:47:55 -0500

    Added comments

--- src/main/java/breakout/Level.java
+++ src/main/java/breakout/Level.java
@@ -1,5 +1,6 @@
 package breakout;
 
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.input.KeyCode;
 
@@ -7,14 +8,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+/**
+ * This class represents an abstraction of a level in Breakout.
+ *
+ * @author Edison Ooi
+ */
 public abstract class Level {
+    // Statistics about level
     public int levelNumber;
     public int numLives;
     public int numRemainingLives;
     public int pointsAccumulated;
 
+    // Indicates whether level has been failed
     public boolean failed;
 
+    // Keep track of which powerups are currently active
     public boolean fastPaddleCheatHasBeenUsed;
     public boolean fastPaddleCheatIsActive;
     public boolean slowBallCheatIsActive;
@@ -22,19 +31,32 @@ public abstract class Level {
     public boolean extraBallIsActive;
     public boolean longPaddleIsActive;
 
+    // Variables needed to render UI components for level
     public String blockConfigFile;
     public Set<Brick> bricks;
     public Group levelRoot;
     public Scoreboard scoreboard;
 
+    // Store width and height of playable region for game
     public int sceneWidth;
     public int sceneHeight;
 
+    /**
+     * Class constructor. Initializes all instance variables and sets up UI elements for level.
+     *
+     * @param levelNumber cardinal number representing current level being created
+     * @param lives number of lives the player starts with
+     * @param root Group object that holds all Nodes rendered by Breakout game
+     * @param sceneWidth width, in pixels, of playable region of Breakout game
+     * @param sceneHeight height, in pixels, of playable region of Breakout game
+     * @param scoreboard Scoreboard object used to display game statistics to player
+     */
     public Level(int levelNumber, int lives, Group root, int sceneWidth, int sceneHeight, Scoreboard scoreboard) {
         this.levelNumber = levelNumber;
         this.numLives = this.numRemainingLives = lives;
         this.pointsAccumulated = 0;
 
+        // Brick configuration text file name must match this format
         this.blockConfigFile = "src/main/resources/level" + levelNumber + "config.txt";
         this.bricks = new HashSet<>();
 
@@ -56,24 +78,63 @@ public abstract class Level {
         setupChildNodes(root, sceneWidth, sceneHeight);
     }
 
-    public abstract void handleKeyInput(KeyCode code);
-    public abstract void step(double elapsedTime);
-
+    /**
+     * Sets up all Nodes needed for level, including bricks, balls, and paddles.
+     *
+     * @param root Group object that holds all Nodes for Breakout game
+     * @param sceneWidth width, in pixels, of playable region of Breakout game
+     * @param sceneHeight height, in pixels, of playable region of Breakout game
+     */
     public abstract void setupChildNodes(Group root, int sceneWidth, int sceneHeight);
 
+    /**
+     * Sets initial properties of all balls in level.
+     */
     public abstract void setupBalls();
 
+    /**
+     * Responds to certain keystrokes such as movement controls or cheat keys.
+     *
+     * @param code KeyCode of key that was pressed
+     */
+    public abstract void handleKeyInput(KeyCode code);
+
+    /**
+     * Performs specific actions when powerup is activated upon brick breaking.
+     *
+     * @param powerup Powerup that was activated
+     */
     public abstract void handlePowerup(Powerup powerup);
 
-    public void setupBricks(Group root, int startX, int startY, int endX, int endY) {
-        Scanner sc;
-        File file;
+    /**
+     * Recalculate all properties of level and its children Nodes.
+     *
+     * @param elapsedTime amount of time since last update
+     */
+    public abstract void step(double elapsedTime);
 
+    /**
+     * Instantiates and renders all bricks for level by extracting brick configuration from given text file and placing
+     * them in given region.
+     *
+     * Should only be called from within setupChildNodes().
+     *
+     * @param root Group object that holds all Nodes for Breakout game
+     * @param startX x-coordinate of starting point of rectangular region for bricks
+     * @param startY y-coordinate of starting point of rectangular region for bricks
+     * @param endX x-coordinate of ending point of rectangular region for bricks
+     * @param endY y-coordinate of ending point of rectangular region for bricks
+     */
+    public void setupBricks(Group root, int startX, int startY, int endX, int endY) {
+        // Attempt to open blockConfigFile, or kill program if file is not found
+        Scanner sc = null;
+        File file = null;
         try {
             file = new File(blockConfigFile);
             sc = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            Platform.exit();
             return;
         }
 
@@ -135,11 +196,18 @@ public abstract class Level {
         sc.close();
     }
 
+    /**
+     * Checks if ball has hit any remaining bricks, and performs the necessary actions if the ball break the brick.
+     * Should only be called within step().
+     *
+     * @param ball
+     */
     public void checkBrickCollisions(Ball ball) {
         for(Brick brick : bricks) {
             if(Breakout.isIntersecting(brick, ball)) {
                 Powerup p = ball.bounce(brick, levelRoot, bricks);
 
+                // If bounce results in the brick breaking it returns the corresponding powerup dropped (could be NONE)
                 if(p != null) {
                     handlePowerup(p);
                     scoreboard.updateScore(brick.getDurability());
@@ -151,6 +219,9 @@ public abstract class Level {
         }
     }
 
+    /**
+     * @return List of active powerups in level
+     */
     public List<String> getActivePowerups() {
         List<String> activePowerups = new ArrayList<>();
 
@@ -177,6 +248,9 @@ public abstract class Level {
         return activePowerups;
     }
 
+    /**
+     * Decreases number of remaining lives and resets ball positions when ball goes off screen.
+     */
     public void loseLife() {
         this.numRemainingLives--;
 
@@ -188,6 +262,9 @@ public abstract class Level {
         setupBalls();
     }
 
+    /**
+     * Resets entire level, including points scored from this level.
+     */
     public void reset() {
         clear();
         setupChildNodes(levelRoot, sceneWidth, sceneHeight);
@@ -205,14 +282,23 @@ public abstract class Level {
         this.fastPaddleCheatIsActive = false;
     }
 
+    /**
+     * Clears all Nodes created by this level.
+     */
     public void clear() {
         levelRoot.getChildren().removeIf(child -> !(child instanceof Scoreboard));
     }
 
+    /**
+     * @return boolean indicating if player failed the level by losing all of their lives
+     */
     public boolean didFail() {
         return this.failed;
     }
 
+    /**
+     * @return boolean indicating if player beat the level by breaking all the bricks
+     */
     public boolean isFinished() {
         return this.bricks.isEmpty();
     }
